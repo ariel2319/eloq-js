2972e93db493e37677f3fd6a315615e85c60222b
Ariel <111529713+ariel2319@users.noreply.github.com>
2024-03-16 21:32:17 +0000

    a mitad del cap3

--- cap3.js
+++ cap3.js
@@ -10,8 +10,27 @@ rl.question('What is your exercises? => ', (numero) => {
     // Convertir la entrada a número si es necesario
     numero = parseFloat(numero);
 
-    if (numero == 1) { }
-    if (numero == 2) { }
+    if (numero == 1) {
+        console.log("MÍNIMO")
+
+        const minimo = (a, b) => {
+            if (a == b) { return ("son idénticos pa") }
+            else {
+                if (a < b) { return a }
+                else { return b }
+            }
+        }
+
+        console.log("Mínimo entre 5 y 5 => ", minimo(5, 5))
+        console.log("Mínimo entre 5 y 7 => ", minimo(7, 7))
+        console.log("Mínimo entre 9 y 7 => ", minimo(9, 7))
+        console.log("Mínimo entre 5 y -1 => ", minimo(5, -1))
+    }
+    if (numero == 2) {
+        console.log("RECURSIÓN")
+
+        
+    }
 
     rl.close();
 });
